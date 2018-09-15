e152c03d95a8de0668243fa0c9821ea299c6e57e
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-09-15 13:51:17 +0530

    Added constructor & direction functions
    Added acceleration and deceleration functionality

--- Cytron.h
+++ Cytron.h
@@ -1,19 +1,42 @@
 class Cytron
-{ public:
-	void init(int z ,int h)
-	{
-		DDRB=(1<<h)|(1<<(h-1));
+{ 
+	public:
+	uint8_t dIrpin, pWmpin,lastpWm=0,step=5,interval=10;	//
+	
+	Cytron(int getdIrpin, int getpWmpin){			//constructor function takes direction and pwm pins for cytron
+		dIrpin=getdIrpin;
+		pWmpin=getpWmpin;
+		pinMode(dIrpin,OUTPUT);			//sets direction pin as output
+	}
+	
+	void direction(int dIrection){
+		digitalWrite(dIrpin,dIrection);
+	}
 
-		if(z==0)
-		{
-		PORTB=(1<<h)|(1<<(h-2));
+	void drive(uint8_t pWm){
+		
+		if(pWm>lastpWm){		//accelerate
+
+			for(lastpWm=lastpWm;lastpWm<pWm,lastpWm+=step){		//increase the speed step by step
+				
+				analogWrite(pWmpin,constraint(lastpWm,0,pWm);
+				delay(interval);		
+			}
+			
 		}
-		if (z==1)
-		{
-			PORTB=(1<<(h-1))|(1<<(h-3));
+
+		else if(pWm<lastpWm){				//decelerate
+
+			for(lastpWm=lastpWm;lastpWm>pWm,lastpWm-=step){		//decrease the speed step by step
+				
+				analogWrite(pWmpin,constraint(lastpWm,pWm,255);
+				delay(interval);		
+			}
 		}
+
+		else{		//no change
+			analogWrite(pWmpin,pWm);
+		}
+		
 	}
-	void drive(int y ,int g)
-	{	analogWrite(y,g);
-	}
-	}
+}
