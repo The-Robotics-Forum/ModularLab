ba9fbc8e38d5ebea5133655d63bc4b36630331e7
Bansole <41703901+Bansole@users.noreply.github.com>
2018-09-21 17:37:37 +0530

    Update Cytron.h

--- Cytron.h
+++ Cytron.h
@@ -1,17 +1,26 @@
 class Cytron
 { 
 	public:
-	uint8_t dIrpin, pWmpin,lastpWm=0,step=5,interval=10;	//
+	uint8_t dIrpin, pWmpin,lastpWm=0,lastpWm1=0,step=5,interval=10,pWmpin1,dIrpin1;	//
 	
 	Cytron(int getdIrpin, int getpWmpin){			//constructor function takes direction and pwm pins for cytron
 		dIrpin=getdIrpin;
 		pWmpin=getpWmpin;
 		pinMode(dIrpin,OUTPUT);			//sets direction pin as output
 	}
-	
+	Cytron(int getdIrpin, int getpWmpin,int gerdIrpin1,int getpWmpin1){     //constructor function takes direction and pwm pins for cytron
+    dIrpin=getdIrpin;
+    pWmpin=getpWmpin;
+    pWmpin1=getpWmpin1;
+    dIrpin=gerdIrpin1;
+    pinMode(dIrpin,OUTPUT);     //sets direction pin as output
+  }
 	void direction(int dIrection){
 		digitalWrite(dIrpin,dIrection);
 	}
+	  void direction(int dIrection){
+    digitalWrite(dIrpin,dIrection);
+  }
 
 	void drive(uint8_t pWm){
 		
@@ -40,3 +49,67 @@ class Cytron
 		
 	}
 }
+void drive(uint8_t pWm,uint8_t pWm1){
+    
+    if(pWm>lastpWm){    //accelerate
+
+      for(lastpWm=lastpWm,lastpWm1=lastpWm1;lastpWm<pWm,lastpWm1<pWm1;){   //increase the sp,eed step by step
+        lastpWm+=step;
+        lastpWm1+=step;
+        analogWrite(pWmpin,constrain(lastpWm,-1,pWm));
+        analogWrite(pWmpin1,constrain(lastpWm1,-1,pWm1));
+        delay(interval);
+            
+      }
+      Serial.println(constrain(lastpWm,-1,pWm));
+      
+    }
+
+    else if((pWm<lastpWm)&&(pWm1<lastpWm1)){       //decelerate
+
+      for(lastpWm=lastpWm,lastpWm1=lastpWm1;lastpWm<pWm,lastpWm1;){   //decrease the speed step by step
+        lastpWm-=step;
+        lastpWm1-=step;
+        analogWrite(pWmpin,constrain(lastpWm,pWm,255));
+         analogWrite(pWmpin1,constrain(lastpWm,pWm,255));
+        delay(interval);    
+    
+      }
+         Serial.println(constrain(lastpWm,pWm,255)); 
+    }
+    
+    
+
+    else{   //no change
+      analogWrite(pWmpin,pWm);
+    }
+    
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
