d1468ddabd0860228f63e1b0ab6b058343b77ec4
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-10-21 15:45:10 +0530

    Added code for 4 motors

--- Cytron.h
+++ Cytron.h
@@ -5,11 +5,12 @@ class Cytron
 	uint8_t dIrpin, pWmpin, lAstpwm, step1, interval;	//variables for first motor
 	uint8_t dIrpin1, pWmpin1, lAstpwm1, step2; //variables for second motor
 	uint8_t dIrpin2, pWmpin2, lAstpwm2, step3; //variables for third motor
+	uint8_t dIrpin3, pWmpin3, lAstpwm3, step4; //variables for third motor
 	
 	Cytron(uint8_t getdIrpin, uint8_t getpWmpin){			//constructor function takes direction and pwm pins for cytron
 		dIrpin=getdIrpin;
 		pWmpin=getpWmpin;
-		pinMode(dIrpin,OUTPUT);			//sets direction pin as output
+					//sets direction pin as output
 		lAstpwm=0;
 		step1=5;
 		interval=10;
@@ -19,15 +20,13 @@ class Cytron
 	    pWmpin=getpWmpin;
 	    pWmpin1=getpWmpin1;
 	    dIrpin1=getdIrpin1;
-	    pinMode(dIrpin,OUTPUT);     //sets direction pin as output
-	    pinMode(dIrpin1,OUTPUT);
 	    lAstpwm=0;
 		lAstpwm1=0;
 		step1=5;
 		step2=5;
 		interval=10;
-		Serial.write(1);
-  }
+
+  	}
   	Cytron(uint8_t getdIrpin, uint8_t getpWmpin, uint8_t getdIrpin1, uint8_t getpWmpin1, uint8_t getdIrpin2, uint8_t getpWmpin2){     //constructor function takes direction and pwm pins for cytron
 	    pWmpin=getpWmpin;
 	    dIrpin=getdIrpin;
@@ -35,9 +34,6 @@ class Cytron
 	    dIrpin1=getdIrpin1;
 	    pWmpin2=getpWmpin2;
 	    dIrpin2=getdIrpin2;
-	    pinMode(dIrpin,OUTPUT);     //sets direction pin as output
-	    pinMode(dIrpin1,OUTPUT);
-	    pinMode(dIrpin2,OUTPUT);
 	    lAstpwm=0;
 		lAstpwm1=0;
 		lAstpwm2=0;
@@ -45,21 +41,57 @@ class Cytron
 		step2=5;
 		step3=5;
 		interval=10;
-  }
+  	}
+  	Cytron(uint8_t getdIrpin, uint8_t getpWmpin, uint8_t getdIrpin1, uint8_t getpWmpin1, uint8_t getdIrpin2, uint8_t getpWmpin2, uint8_t getdIrpin3, uint8_t getpWmpin3){     //constructor function takes direction and pwm pins for cytron
+	    pWmpin=getpWmpin;
+	    dIrpin=getdIrpin;
+	    pWmpin1=getpWmpin1;
+	    dIrpin1=getdIrpin1;
+	    pWmpin2=getpWmpin2;
+	    dIrpin2=getdIrpin2;
+	    pWmpin3=getpWmpin3;
+	    dIrpin3=getdIrpin3;
+	    lAstpwm=0;
+		lAstpwm1=0;
+		lAstpwm2=0;
+		lAstpwm3=0;
+		step1=5;
+		step2=5;
+		step3=5;
+		step4=5;
+		interval=10;
+  	}
+
   	void direction(uint8_t dIrection){
+  		pinMode(dIrpin,OUTPUT);
 		digitalWrite(dIrpin,dIrection);
 	}
 	
 	void direction(uint8_t dIrection, uint8_t dIrection1){
+		pinMode(dIrpin,OUTPUT);     //sets direction pin as output
+	    pinMode(dIrpin1,OUTPUT);
 		digitalWrite(dIrpin,dIrection);
 		digitalWrite(dIrpin1,dIrection1);
-		Serial.write(2);
 	}
 
 	void direction(uint8_t dIrection, uint8_t dIrection1, uint8_t dIrection2){
+		pinMode(dIrpin,OUTPUT);     //sets direction pin as output
+	    pinMode(dIrpin1,OUTPUT);
+	    pinMode(dIrpin2,OUTPUT);
 		digitalWrite(dIrpin,dIrection);
 		digitalWrite(dIrpin1,dIrection1);
 		digitalWrite(dIrpin2,dIrection2);
+	}
+
+	void direction(uint8_t dIrection, uint8_t dIrection1, uint8_t dIrection2, uint8_t dIrection3){
+		pinMode(dIrpin,OUTPUT);     //sets direction pin as output
+	    pinMode(dIrpin1,OUTPUT);
+	    pinMode(dIrpin2,OUTPUT);
+	    pinMode(dIrpin3,OUTPUT);
+		digitalWrite(dIrpin,dIrection);
+		digitalWrite(dIrpin1,dIrection1);
+		digitalWrite(dIrpin2,dIrection2);
+		digitalWrite(dIrpin3,dIrection3);
 	}
 
 	void drive(uint8_t pWm){
@@ -90,7 +122,6 @@ class Cytron
 
 	//TWO MOTORS
 	void drive(uint8_t pWm, uint8_t pWm1){
-		
 		if(pWm>lAstpwm||pWm1>lAstpwm1){		//accelerate
 
 			for(lAstpwm=lAstpwm,lAstpwm1=lAstpwm1;lAstpwm<pWm||lAstpwm1<pWm1;){		//increase the speed step by step
@@ -102,7 +133,7 @@ class Cytron
 					lAstpwm1+=step2;
 					analogWrite(pWmpin1,constrain(lAstpwm1,0,pWm1));
 				}	
-				delay(interval);		
+				delay(interval);
 			}
 			
 		}
@@ -118,15 +149,14 @@ class Cytron
 					lAstpwm1-=step2;
 					analogWrite(pWmpin1,constrain(lAstpwm1,pWm1,255));
 				}
-				delay(interval);		
+				delay(interval);	
 			}
 		}
 
 		else{		//no change
 			analogWrite(pWmpin,pWm);
 			analogWrite(pWmpin1,pWm1);
-		}	
-		Serial.write(3);
+		}
 	}
 
 	//THREE MOTORS
@@ -152,9 +182,9 @@ class Cytron
 			
 		}
 
-		else if(pWm<lAstpwm||pWm1<lAstpwm1){				//decelerate
+		else if(pWm<lAstpwm||pWm1<lAstpwm1||pWm2<lAstpwm2){				//decelerate
 
-			for(lAstpwm=lAstpwm,lAstpwm1=lAstpwm1;lAstpwm>pWm||lAstpwm1>pWm1;){		//decrease the speed step by step
+			for(lAstpwm=lAstpwm,lAstpwm1=lAstpwm1,lAstpwm2=lAstpwm2;lAstpwm>pWm||lAstpwm1>pWm1||lAstpwm2>pWm2;){		//decrease the speed step by step
 				if (pWm<lAstpwm){
 					lAstpwm-=step1;
 					analogWrite(pWmpin,constrain(lAstpwm,pWm,255));
@@ -174,59 +204,125 @@ class Cytron
 		else{		//no change
 			analogWrite(pWmpin,pWm);
 			analogWrite(pWmpin1,pWm1);
-			analogWrite(pWmpin1,pWm2);
+			analogWrite(pWmpin2,pWm2);
 		}	
 	}	
-		void stop(short int s)
-	{ switch(s)
-		{case 1:
-			analogWrite(pWmpin,0);
+	
+	//FOUR MOTORS
+	void drive(uint8_t pWm, uint8_t pWm1, uint8_t pWm2, uint8_t pWm3){
+		
+		if(pWm>lAstpwm||pWm1>lAstpwm1||pWm2>lAstpwm2||pWm3>lAstpwm3){		//accelerate
+
+			for(lAstpwm=lAstpwm,lAstpwm1=lAstpwm1,lAstpwm2=lAstpwm2,lAstpwm3=lAstpwm3;lAstpwm<pWm||lAstpwm1<pWm1||lAstpwm2<pWm2||lAstpwm3<pWm3;){		//increase the speed step by step
+				if (pWm>lAstpwm){
+					lAstpwm+=step1;
+					analogWrite(pWmpin,constrain(lAstpwm,0,pWm));
+				}
+				if (pWm1>lAstpwm1){
+					lAstpwm1+=step2;
+					analogWrite(pWmpin1,constrain(lAstpwm1,0,pWm1));
+				}
+				if (pWm2>lAstpwm2){
+					lAstpwm2+=step3;
+					analogWrite(pWmpin2,constrain(lAstpwm2,0,pWm2));
+				}
+				if (pWm3>lAstpwm3){
+					lAstpwm3+=step4;
+					analogWrite(pWmpin3,constrain(lAstpwm3,0,pWm3));
+				}	
+				delay(interval);		
+			}
+			
+		}
+
+		else if(pWm<lAstpwm||pWm1<lAstpwm1||pWm2<lAstpwm2||pWm3<lAstpwm3){				//decelerate
+
+			for(lAstpwm=lAstpwm,lAstpwm1=lAstpwm1,lAstpwm2=lAstpwm2,lAstpwm3=lAstpwm3;lAstpwm>pWm||lAstpwm1>pWm1||lAstpwm2>pWm2||lAstpwm3>pWm3;){		//decrease the speed step by step
+				if (pWm<lAstpwm){
+					lAstpwm-=step1;
+					analogWrite(pWmpin,constrain(lAstpwm,pWm,255));
+				}
+				if (pWm1<lAstpwm1){
+					lAstpwm1-=step2;
+					analogWrite(pWmpin1,constrain(lAstpwm1,pWm1,255));
+				}
+				if (pWm2<lAstpwm2){
+					lAstpwm2-=step3;
+					analogWrite(pWmpin2,constrain(lAstpwm2,pWm2,255));
+				}
+				if (pWm3<lAstpwm3){
+					lAstpwm3-=step4;
+					analogWrite(pWmpin3,constrain(lAstpwm3,pWm3,255));
+				}
+				delay(interval);		
+			}
+		}
+
+		else{		//no change
+			analogWrite(pWmpin,pWm);
+			analogWrite(pWmpin1,pWm1);
+			analogWrite(pWmpin2,pWm2);
+			analogWrite(pWmpin3,pWm3);
+		}	
+	}
+
+	void stop(int s)
+	{ 
+		switch(s)
+		{
+			case 1:
+				analogWrite(pWmpin,0);
 		    break;
-	     case 2:
-		 analogWrite(pWmpin1,0);
+		    
+		    case 2:
+				analogWrite(pWmpin1,0);
 		    break;
-		case 3:
-		analogWrite(pWmpin2,0);
+			
+			case 3:
+				analogWrite(pWmpin2,0);
 		    break;	
+		}
 	}
-	}
+	
+
 	void stop(short int s,short int a)
-	{ switch(s)
-{    case 1:
-			analogWrite(pWmpin,0);
-                   switch(a)
-			   {case 2:
-			    analogWrite(pWmpin1,0);
-		       break;
-		     case 3:
-	         	analogWrite(pWmpin2,0);
-		       break;	
-			    }		   
-	              break;
-	case 2:
-			 analogWrite(pWmpin1,0);
-			  switch(a)
-			   {case 1:
-			    analogWrite(pWmpin,0);
-		       break;
-		     case 3:
-	         	analogWrite(pWmpin2,0);
-		       break;	
-			    }		   
-	              break;
-	case 3:
-	           analogWrite(pWmpin2,0);
-			     switch(a)
-			   {case 1:
-			    analogWrite(pWmpin,0);
-		       break;
-		     case 2:
-	         	analogWrite(pWmpin1,0);
-		       break;	
-			    }		   
-	            	
-		
-		
+	{ 
+		switch(s)
+		{    
+			case 1:
+					analogWrite(pWmpin,0);
+		                   switch(a)
+					   {case 2:
+					    analogWrite(pWmpin1,0);
+				       break;
+				     case 3:
+			         	analogWrite(pWmpin2,0);
+				       break;	
+					    }		   
+			              break;
+			case 2:
+					 analogWrite(pWmpin1,0);
+					  switch(a)
+					   {case 1:
+					    analogWrite(pWmpin,0);
+				       break;
+				     case 3:
+			         	analogWrite(pWmpin2,0);
+				       break;	
+					    }		   
+			              break;
+			case 3:
+		           analogWrite(pWmpin2,0);
+				   switch(a)
+				   {
+				   	case 1:
+				    	analogWrite(pWmpin,0);
+			       	break;
+			     
+			     	case 2:
+		         		analogWrite(pWmpin1,0);
+			       	break;	
+				    }		   
 		 }
 	}	
 };
